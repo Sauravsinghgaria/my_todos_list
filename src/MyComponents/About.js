3a736c8e459721e2ca2c213f7234d82c438de3a6
saurav singh garia <91400696+Sauravsinghgaria@users.noreply.github.com>
2022-08-23 13:20:32 +0530

    React todos_list project

---
+++ src/MyComponents/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis iusto, cupiditate velit voluptatem hic quisquam voluptatibus, excepturi mollitia corrupti fuga deserunt. Nam quas libero odio aperiam laboriosam delectus quibusdam repellendus!
+        </p>
+    </div>
+  )
+}
